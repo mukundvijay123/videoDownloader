db0a5c26a3977d09b2b50383c9c2ba72037b9de9
mukundvijay123 <115480143+mukundvijay123@users.noreply.github.com>
2024-07-25 16:29:59 +0530

    Update setup.sh
    Convinient way to run setup for linux

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+git clone https://github.com/mukundvijay123/videoDownloader.git
+cd videoDownloader
+python -m venv MyVenv
+source MyVenv/bin/activate
+pip install -r requirements.txt
+
 
